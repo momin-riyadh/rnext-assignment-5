db283393362224ad9297c0438d4e9984c5699198
Momin Riyadh <mominriyadh@gmail.com>
2024-03-11 12:49:47 +0600

    fix and deploy  router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import {Routes, Route} from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 
 function App() {
 
     return (
         <Routes>
             <Route element={<HomePage/>} path="/" exact/>
+            <Route element={<LoginPage/>} path="/login"/>
+            <Route element={<HomePage/>} path="/"/>
+            <Route element={<HomePage/>} path="/"/>
+            <Route element={<HomePage/>} path="/"/>
         </Routes>
     )
 }
